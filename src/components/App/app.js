73f1f2aa3ea7da999232fb40c6bf006931f70db0
Rzva03 <avictor.zrodriguez@gmail.com>
2023-10-04 21:01:29 -0600

    feat: create theme context

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,8 +1,18 @@
+import React, { useContext } from 'react'
+import { ThemeContext } from '../../context/ThemeContext';
 
-function App() {
+const App = () => {
+    const { state } = useContext(ThemeContext);
+    const { theme } = state;
+    const isLightTheme = theme === 'light';
+
     return (
-        <h1 className='text-7xl text-red-900'>Hello wey!!</h1>
-    );
+        <div className={`w-screen h-screen ${isLightTheme ? 'bg-white' : 'bg-black'}`}>
+            <h1 className={`text-8xl ${isLightTheme ? 'text-black' : 'text-white'}`}>
+                Hello
+            </h1>
+        </div>
+    )
 }
 
 export default App;
---
+++ src/context/ThemeContext.js
@@ -0,0 +1,28 @@
+import { createContext, useReducer } from "react";
+
+export const ThemeContext = createContext();
+
+const initialState = {
+    theme: 'light'
+}
+
+const reducer = (state, newState) => {
+    return { state, ...newState }
+}
+
+const ThemeProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const valueProvider = {
+        state,
+        dispatch
+    }
+    return (
+        <ThemeContext.Provider
+            value={valueProvider}
+        >
+            {children}
+        </ThemeContext.Provider>
+    )
+}
+
+export default ThemeProvider;
