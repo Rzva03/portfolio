6886d5d8f1ccf24d44971827cafc8c19349e11d4
Rzva03 <avictor.zrodriguez@gmail.com>
2023-10-06 12:17:44 -0600

    feat: add new imports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './components/App/app';
 import ThemeProvider from './context/ThemeContext';
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import './index.css';
+import '../src/assets/css/global.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
