0091da7454d1e2e8b33dd8dc9ee052a224d9a8b0
Rzva03 <avictor.zrodriguez@gmail.com>
2023-10-07 16:10:50 -0600

    fix: update styles

--- src/components/MenuMobile/menuMobile.js
+++ src/components/MenuMobile/menuMobile.js
@@ -4,20 +4,28 @@ import MenuContent from '../MenuContent/menuContent';
 import { Button } from 'primereact/button';
 import useMenuMobile from './useMenuMobile';
 import ToggleTheme from '../ToggleTheme/toggleTheme';
-// eslint-disable-next-line no-unused-vars
 import defaultStyles from './menuMobile.module.css';
 
 const MenuMobile = (props) => {
     const {
+        isLightTheme,
         isMenuMobileVisible,
         setIsMenuMobileVisible
     } = useMenuMobile();
     return (
         <div className='menuMobile flex justify-between'>
-            <Sidebar visible={isMenuMobileVisible} onHide={() => setIsMenuMobileVisible(false)}>
-                <MenuContent {...props} />
+            <Sidebar
+                visible={isMenuMobileVisible}
+                onHide={() => setIsMenuMobileVisible(false)}
+                className={`${isLightTheme ? 'lightTheme' : 'darkTheme'}`}
+            >
+                <MenuContent {...props} setIsMenuMobileVisible={setIsMenuMobileVisible}/>
             </Sidebar>
-            <Button icon="pi pi-bars" className="p-button-rounded p-button-text flex justify-start" onClick={() => setIsMenuMobileVisible(true)} />
+            <Button
+                icon="pi pi-bars"
+                className={`p-button-rounded p-button-text flex justify-start ${isLightTheme ? defaultStyles.menuLight : defaultStyles.menuDark}`}
+                onClick={() => setIsMenuMobileVisible(true)}
+            />
             <ToggleTheme />
         </div>
     )
