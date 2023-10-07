7ef8954c4fc6bb4cd3c5e41a24cec9e0e47e1465
Rzva03 <avictor.zrodriguez@gmail.com>
2023-10-07 16:11:41 -0600

    feat: close the menu when is change a option menu

--- src/components/MenuMobile/useMenuMobile.js
+++ src/components/MenuMobile/useMenuMobile.js
@@ -1,9 +1,14 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { ThemeContext } from "../../context/ThemeContext";
 
 const useMenuMobile = () => {
     const [isMenuMobileVisible, setIsMenuMobileVisible] = useState(false);
+    const { state } = useContext(ThemeContext);
+    const { theme } = state;
+    const isLightTheme = theme === 'light';
 
     return {
+        isLightTheme,
         isMenuMobileVisible,
         setIsMenuMobileVisible
     }
