a93ba209ac31cc7456432776bd2394df47612e22
Rzva03 <avictor.zrodriguez@gmail.com>
2023-10-11 13:40:15 -0600

    fix: add new style for skeleton

--- src/components/Skeleton/skeleton.js
+++ src/components/Skeleton/skeleton.js
@@ -1,31 +1,32 @@
 import React, { Fragment } from 'react'
+import defaultStyles from './skeleton.module.css';
 
 const Skeleton = () => {
     return (
         <Fragment>
             <div className="animate-pulse flex space-x-4">
-                <div className="rounded bg-slate-300 h-44 w-64"></div>
+                <div className={`rounded bg-slate-300 h-44 w-64 ${defaultStyles.skeleton}`}></div>
             </div>
             <div className="animate-pulse flex space-x-4">
-                <div className="rounded bg-slate-300 h-44 w-64"></div>
+                <div className={`rounded bg-slate-300 h-44 w-64 ${defaultStyles.skeleton}`}></div>
             </div>
             <div className="animate-pulse flex space-x-4">
-                <div className="rounded bg-slate-300 h-44 w-64"></div>
+                <div className={`rounded bg-slate-300 h-44 w-64 ${defaultStyles.skeleton}`}></div>
             </div>
             <div className="animate-pulse flex space-x-4">
-                <div className="rounded bg-slate-300 h-44 w-64"></div>
+                <div className={`rounded bg-slate-300 h-44 w-64 ${defaultStyles.skeleton}`}></div>
             </div>
             <div className="animate-pulse flex space-x-4">
-                <div className="rounded bg-slate-300 h-44 w-64"></div>
+                <div className={`rounded bg-slate-300 h-44 w-64 ${defaultStyles.skeleton}`}></div>
             </div>
             <div className="animate-pulse flex space-x-4">
-                <div className="rounded bg-slate-300 h-44 w-64"></div>
+                <div className={`rounded bg-slate-300 h-44 w-64 ${defaultStyles.skeleton}`}></div>
             </div>
             <div className="animate-pulse flex space-x-4">
-                <div className="rounded bg-slate-300 h-44 w-64"></div>
+                <div className={`rounded bg-slate-300 h-44 w-64 ${defaultStyles.skeleton}`}></div>
             </div>
         </Fragment>
-        
+
     )
 }
 
