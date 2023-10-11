73be33e47bceae00fa9032b09d226d2260f4f5e8
Rzva03 <avictor.zrodriguez@gmail.com>
2023-10-11 12:22:00 -0600

    fix: change correct props

--- src/components/Skill/skillSkeleton.js
+++ src/components/Skill/skillSkeleton.js
@@ -3,26 +3,26 @@ import React, { Fragment } from 'react'
 const SkillSkeleton = () => {
     return (
         <Fragment>
-            <div class="animate-pulse flex space-x-4">
-                <div class="rounded bg-slate-300 h-44 w-64"></div>
+            <div className="animate-pulse flex space-x-4">
+                <div className="rounded bg-slate-300 h-44 w-64"></div>
             </div>
-            <div class="animate-pulse flex space-x-4">
-                <div class="rounded bg-slate-300 h-44 w-64"></div>
+            <div className="animate-pulse flex space-x-4">
+                <div className="rounded bg-slate-300 h-44 w-64"></div>
             </div>
-            <div class="animate-pulse flex space-x-4">
-                <div class="rounded bg-slate-300 h-44 w-64"></div>
+            <div className="animate-pulse flex space-x-4">
+                <div className="rounded bg-slate-300 h-44 w-64"></div>
             </div>
-            <div class="animate-pulse flex space-x-4">
-                <div class="rounded bg-slate-300 h-44 w-64"></div>
+            <div className="animate-pulse flex space-x-4">
+                <div className="rounded bg-slate-300 h-44 w-64"></div>
             </div>
-            <div class="animate-pulse flex space-x-4">
-                <div class="rounded bg-slate-300 h-44 w-64"></div>
+            <div className="animate-pulse flex space-x-4">
+                <div className="rounded bg-slate-300 h-44 w-64"></div>
             </div>
-            <div class="animate-pulse flex space-x-4">
-                <div class="rounded bg-slate-300 h-44 w-64"></div>
+            <div className="animate-pulse flex space-x-4">
+                <div className="rounded bg-slate-300 h-44 w-64"></div>
             </div>
-            <div class="animate-pulse flex space-x-4">
-                <div class="rounded bg-slate-300 h-44 w-64"></div>
+            <div className="animate-pulse flex space-x-4">
+                <div className="rounded bg-slate-300 h-44 w-64"></div>
             </div>
         </Fragment>
         
