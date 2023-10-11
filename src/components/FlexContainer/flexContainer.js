12fc781de3e5d5c408c0d1ab8401fee635601595
Rzva03 <avictor.zrodriguez@gmail.com>
2023-10-11 13:16:30 -0600

    feat: create flexContainer cpm

---
+++ src/components/FlexContainer/flexContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FlexContainer = (props) => {
+    const { children } = props;
+    return (
+        <div className='flex space-y-6 flex-col md:flex-row md:justify-center md:flex-wrap md:gap-3 md:space-y-0 pb-10 md:pb-0'>
+            {children}
+        </div>
+    )
+}
+
+export default FlexContainer;
