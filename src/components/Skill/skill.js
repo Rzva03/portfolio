172191db6399bd0f2d7ced7adcd8d0f185d6ddac
Rzva03 <avictor.zrodriguez@gmail.com>
2023-10-11 13:17:04 -0600

    fix: add FlexContainer cpm

--- src/components/Skill/skill.js
+++ src/components/Skill/skill.js
@@ -2,8 +2,9 @@ import React from 'react';
 import technologies from '../../data/technologies.json';
 import Technology from '../Technology/technology';
 import useSkill from './useSkill';
-import SkillSkeleton from './skillSkeleton';
+import SkillSkeleton from '../Skeleton/skeleton';
 import Title from '../Title/title';
+import FlexContainer from '../FlexContainer/flexContainer';
 
 const Skill = () => {
     const {
@@ -15,8 +16,8 @@ const Skill = () => {
         <div
             className={`h-full flex flex-col md:justify-center items-center space-y-5 w-full font-semibold ${isLightTheme ? 'lightTheme' : 'darkTheme'}`}
         >
-            <Title primaryText='My' secondaryText='Skill' as="h1"/>
-            <div className='flex space-y-6 flex-col md:flex-row md:justify-center md:flex-wrap md:gap-3 md:space-y-0 pb-10 md:pb-0'>
+            <Title primaryText='My' secondaryText='Skills' as="h1" />
+            <FlexContainer>
                 {isLoading ? (
                     <SkillSkeleton />
                 ) : (
@@ -28,7 +29,7 @@ const Skill = () => {
                         />
                     )))
                 )}
-            </div>
+            </FlexContainer>
         </div>
     )
 }
