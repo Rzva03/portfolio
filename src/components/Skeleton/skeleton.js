15a210a331b8f333efbc108585a35948fe494636
Rzva03 <avictor.zrodriguez@gmail.com>
2023-10-11 15:59:50 -0600

    feat: add new styles for skeleton cpm

--- src/components/Skeleton/skeleton.js
+++ src/components/Skeleton/skeleton.js
@@ -5,25 +5,25 @@ const Skeleton = () => {
     return (
         <Fragment>
             <div className="animate-pulse flex space-x-4">
-                <div className={`rounded bg-slate-300 h-44 w-64 ${defaultStyles.skeleton}`}></div>
+                <div className={`rounded bg-slate-300 h-44 w-64 md:w-48 md:h-24 lg:h-44 lg:w-64 ${defaultStyles.skeleton}`}></div>
             </div>
             <div className="animate-pulse flex space-x-4">
-                <div className={`rounded bg-slate-300 h-44 w-64 ${defaultStyles.skeleton}`}></div>
+                <div className={`rounded bg-slate-300 h-44 w-64 md:w-48 md:h-24 lg:h-44 lg:w-64 ${defaultStyles.skeleton}`}></div>
             </div>
             <div className="animate-pulse flex space-x-4">
-                <div className={`rounded bg-slate-300 h-44 w-64 ${defaultStyles.skeleton}`}></div>
+                <div className={`rounded bg-slate-300 h-44 w-64 md:w-48 md:h-24 lg:h-44 lg:w-64 ${defaultStyles.skeleton}`}></div>
             </div>
             <div className="animate-pulse flex space-x-4">
-                <div className={`rounded bg-slate-300 h-44 w-64 ${defaultStyles.skeleton}`}></div>
+                <div className={`rounded bg-slate-300 h-44 w-64 md:w-48 md:h-24 lg:h-44 lg:w-64 ${defaultStyles.skeleton}`}></div>
             </div>
             <div className="animate-pulse flex space-x-4">
-                <div className={`rounded bg-slate-300 h-44 w-64 ${defaultStyles.skeleton}`}></div>
+                <div className={`rounded bg-slate-300 h-44 w-64 md:w-48 md:h-24 lg:h-44 lg:w-64 ${defaultStyles.skeleton}`}></div>
             </div>
-            <div className="animate-pulse flex space-x-4">
-                <div className={`rounded bg-slate-300 h-44 w-64 ${defaultStyles.skeleton}`}></div>
+            <div className="animate-pulse flex space-x-4 lg:hidden">
+                <div className={`rounded bg-slate-300 h-44 w-64 md:w-48 md:h-24 lg:h-48 lg:w-72 ${defaultStyles.skeleton}`}></div>
             </div>
-            <div className="animate-pulse flex space-x-4">
-                <div className={`rounded bg-slate-300 h-44 w-64 ${defaultStyles.skeleton}`}></div>
+            <div className="animate-pulse flex space-x-4 md:hidden">
+                <div className={`rounded bg-slate-300 h-44 w-64 md:w-48 md:h-24 lg:h-48 lg:w-72 ${defaultStyles.skeleton}`}></div>
             </div>
         </Fragment>
 
