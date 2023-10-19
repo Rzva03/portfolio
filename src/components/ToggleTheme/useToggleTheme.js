f7d7e21a0566cc58d0cca3af24aea9275b5547fe
Rzva03 <avictor.zrodriguez@gmail.com>
2023-10-19 13:38:26 -0600

    feat: add theme value to localStorage

--- src/components/ToggleTheme/useToggleTheme.js
+++ src/components/ToggleTheme/useToggleTheme.js
@@ -2,13 +2,25 @@ import { useContext, useEffect, useState } from "react";
 import { ThemeContext } from "../../context/ThemeContext";
 import useCheckMobileScreen from "../../hooks/useCheckMobileScreen";
 
+const THEME_LIGHT = 'light';
+
 const useToggleTheme = () => {
     const [isChecked, setIsChecked] = useState(false);
     const { dispatch } = useContext(ThemeContext);
+    const themeFromLocalStorage = localStorage.getItem('theme') ?? THEME_LIGHT;
+
     const {
         isMobile
     } = useCheckMobileScreen();
 
+    useEffect(() => {
+      if (themeFromLocalStorage === THEME_LIGHT) {
+        setIsChecked(false);
+        return;
+      }
+      setIsChecked(true);
+    }, [themeFromLocalStorage])
+    
     useEffect(() => {
         if (isChecked) {
             dispatch({ theme: 'dark' });
--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -2,8 +2,11 @@ import { createContext, useEffect, useReducer } from "react";
 
 export const ThemeContext = createContext();
 
+const THEME_LIGHT = 'light';
+const THEME_DARK = 'dark';
+
 const initialState = {
-    theme: 'light',
+    theme: THEME_LIGHT,
     isAutoHeight: false,
     isLightTheme: true
 }
@@ -11,9 +14,6 @@ const initialState = {
 const reducer = (state, newState) => {
     return { state, ...newState }
 }
-
-const THEME_LIGHT = 'light';
-const THEME_DARK = 'dark';
 
 const ThemeProvider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, initialState);
@@ -25,15 +25,17 @@ const ThemeProvider = ({ children }) => {
 
     useEffect(() => {
         if (state.theme === THEME_LIGHT) {
-            dispatch({isLightTheme: true});
+            dispatch({ isLightTheme: true });
+            localStorage.setItem('theme', THEME_LIGHT);
             return;
         }
         if (state.theme === THEME_DARK) {
-            dispatch({isLightTheme: false});
+            dispatch({ isLightTheme: false });
+            localStorage.setItem('theme', THEME_DARK);
             return;
         }
     }, [state.theme]);
-    
+
     return (
         <ThemeContext.Provider
             value={valueProvider}
